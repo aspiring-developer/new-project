de8611cef7e60c3425a82e654d253c964449c4c9
ssh1sharma <emailtotheowner@gmail.com>
2021-03-29 12:51:21 -0400

    Login page routing WIP

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -2,46 +2,47 @@
 const User = require('../models/User');
 
 // Creating controllers for pages: these are exported to use in router.js
-exports.indexController = function(req, res) {
+exports.indexController = function (req, res) {
   res.render('index')
 };
 
-exports.webController = function(req, res) {
+exports.webController = function (req, res) {
   res.render('web-development')
 };
 
-exports.graphicController = function(req, res) {
+exports.graphicController = function (req, res) {
   res.render('graphic-digital')
 };
 
-exports.paintingController = function(req, res) {
+exports.paintingController = function (req, res) {
   res.render('painting')
 };
 
-exports.aboutController = function(req, res) {
+exports.aboutController = function (req, res) {
   res.render('about')
 };
 
 // Creating an instance of the User model based on user entered req.body
-exports.registerController = function(req,res) {
+exports.registerController = function (req, res) {
   console.log(req.body);
-let user = new User(req.body);
-user.registerUser();
-  if(user.errorMessage.length) {
+  let user = new User(req.body);
+  user.registerUser();
+  if (user.errorMessage.length) {
     res.send(user.errorMessage);
   } else {
-    res.render("modal")
+    res.render("pop-up");
   }
 }
 
 // Creating an instance of the User model based on user entered req.body
-exports.loginController = function(req,res) {
-let user = new User(req.body);
-user.loginUser();
-  if(user.errorMessage.length) {
-    res.send(user.errorMessage);
-  } else {
-    res.render("painting")
-  }
+exports.loginController = function (req, res) {
+  let user = new User(req.body);
+  user.loginUser();
+
+  //if(user.errorMessage.length) {
+  //  res.send(user.errorMessage);
+  //} else {
+  //  res.render("painting")
+  //}
 }
 
--- models/User.js
+++ models/User.js
@@ -8,22 +8,9 @@ let User = function (formData) {
   this.errorMessage = [];
 }
 
-User.prototype.validate = function () {
-  // Username validation condition
-  if (this.userInput.username == "") { this.errorMessage.push("Username is required!") };
-  if (this.userInput.username.length > 0 && this.userInput.username.length < 3) { this.errorMessage.push("Username must be at least 3 characters long.") };
-  if (this.userInput.username.length > 20) { this.errorMessage.push("Username must not be over 20 characters.") };
-  if (this.userInput.username !== "" && !validator.isAlphanumeric(this.userInput.username)) { this.errorMessage.push("Username can only contain letters and numbers.") };
+User.prototype.cleanUp = function () {
   if (typeof (this.userInput.username) != "string") { this.userInput.username = "" };
-
-  // Email validation condition
-  if (!validator.isEmail(this.userInput.email)) { this.errorMessage.push("A valid email is required!") };
   if (typeof (this.userInput.email) != "string") { this.userInput.email = "" };
-
-  // Password validation condition
-  if (this.userInput.password == "") { this.errorMessage.push("Password is required!") };
-  if (this.userInput.password.length > 0 && this.userInput.password.length < 6) { this.errorMessage.push("Password must be at least 6 characters long.") };
-  if (this.userInput.password.length > 20) { this.errorMessage.push("Password must not be over 20 characters.") };
   if (typeof (this.userInput.password) != "string") { this.userInput.password = "" };
 
   // This block below resets/updates the fields to have only wanted characters/texts/objects/etc (as validated codes above). And also trims and makes lower case of username and email field texts.
@@ -34,25 +21,40 @@ User.prototype.validate = function () {
   }
 }
 
+User.prototype.validate = function () {
+  // Username validation condition
+  if (this.userInput.username == "") { this.errorMessage.push("Username is required!") };
+  if (this.userInput.username.length > 0 && this.userInput.username.length < 3) { this.errorMessage.push("Username must be at least 3 characters long.") };
+  if (this.userInput.username.length > 20) { this.errorMessage.push("Username must not be over 20 characters.") };
+  if (this.userInput.username !== "" && !validator.isAlphanumeric(this.userInput.username)) { this.errorMessage.push("Username can only contain letters and numbers.") };
+
+  // Email validation condition
+  if (!validator.isEmail(this.userInput.email)) { this.errorMessage.push("A valid email is required!") };
+
+  // Password validation condition
+  if (this.userInput.password == "") { this.errorMessage.push("Password is required!") };
+  if (this.userInput.password.length > 0 && this.userInput.password.length < 6) { this.errorMessage.push("Password must be at least 6 characters long.") };
+  if (this.userInput.password.length > 20) { this.errorMessage.push("Password must not be over 20 characters.") };
+}
+
+User.prototype.loginUser = function () {
+  this.cleanUp();
+  userEnteredData.collection("personal_site_collection").findOne({username: this.userInput.username}), (err, userWhoIsLogging) => {
+if(userWhoIsLogging && userWhoIsLogging.password == this.userInput.password) {
+   console.log("successfully login!")
+} else {
+  console.log("Invalid username or password!");
+}
+  }
+}
+
 User.prototype.registerUser = function () {
-  //this.cleanUp();
+  this.cleanUp();
   this.validate();
   if (!this.errorMessage.length) {
     userEnteredData.collection("personal_site_collection").insertOne(this.userInput)
   };
 }
-
-User.prototype.loginUser = function () {
-  this.validate();
-  userEnteredData.collection("personal_site_collection").findOne({username: this.userInput.username}, (err, userWhoIsLogging) => {
-if(userWhoIsLogging && userWhoIsLogging.password == this.userInput.password) {
-console.log("Successfully login")
-} else {
-console.log("Wrong UN or PW!")
-}
-  })}
-
-
 
 // Exporting User model to use in registerController module in PageController file
 module.exports = User;
